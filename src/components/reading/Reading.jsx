e4c116d5469942f9d3f04d0ff3f085bba5b353f9
theopin <theopinto98@hotmail.com>
2023-09-24 18:17:41 +0800

    Fix formatting of readings

--- src/components/reading/Reading.jsx
+++ src/components/reading/Reading.jsx
@@ -6,8 +6,12 @@ import Header from "../header/Header";
 function Reading(props) {
   // Component code here
 
-  const paragraphs = splitTextIntoParagraphs((props.text + "\n").split(".\n"));
-  // console.log(props.text)
+    const paragraphs = splitTextIntoParagraphs(
+      props.text
+        .replace(/\n/g, ' ')
+        .split(/(?<=[.!?] |[.!?]["']|[.!?]['"])/)
+        .filter(sentence => sentence.trim() !== ''));
+
 
   const formattedParagraphs = paragraphs.map((paragraph, index) => (
     <p key={props.title+index} className="content">
@@ -29,7 +33,7 @@ function Reading(props) {
 function splitTextIntoParagraphs(sentences) {
   const newParagraphs = []
   let currParagraph = ""
-  console.log(sentences)
+
   sentences.forEach((sentence, index) => {
     if(sentence === "") {
       newParagraphs.push(currParagraph.trimEnd());
@@ -37,27 +41,21 @@ function splitTextIntoParagraphs(sentences) {
     }
 
     // Word count of current para at most 50 or curr para is not in middle of a inner dialogue
-    if ((currParagraph + sentence).split(" ").length <= 50 || (currParagraph.split("'").length - 1) % 2 !== 0) {
-      console.log(1, index, sentences.length, sentence)
-      currParagraph += sentence.endsWith("\n") ? sentence : sentence + ". ";
+    if ((currParagraph + sentence).split(" ").length <= 90) {
+      currParagraph += sentence + " ";
 
       if (index === sentences.length - 1) {
-        console.log(11, currParagraph.split(" ").length - 1)
         newParagraphs.push(currParagraph.trimEnd());
       }
     }
     
     else {
       if (index === sentences.length - 1) {
-        console.log(2, index, sentences.length, sentence)
-        currParagraph += sentence.endsWith("\n") ? sentence : sentence + ". ";
-        console.log(currParagraph.split(" ").length - 1)
+        currParagraph += sentence + " ";
         newParagraphs.push(currParagraph.trimEnd());
       } else {
-        console.log(22, index, sentences.length, sentence)
-        console.log(currParagraph.split(" ").length - 1)
         newParagraphs.push(currParagraph.trimEnd());
-        currParagraph = sentence.endsWith("\n") ? sentence : sentence + ". ";
+        currParagraph = sentence + " ";
       }
     }
     
