d4818db936d24c9f07fb7474752da907c8c434bf
theopin <theopinto98@hotmail.com>
2023-09-25 10:41:38 +0800

    Edit issue with header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ function Header(props) {
     <div>
           <div className="header">
         <h3 className="title">{props.title}</h3>
-        <h5 className="verse">{props.verse.replace(/\bcf\.\s+/gi, "")}</h5>
+        <h5 className="verse">{props.verse.replace(/(\D)(\d)/, '$1 $2').replace(/\bcf\.\s+/gi, "")}</h5>
       </div>
       <h5 className="summary">{props.summary}</h5>
     </div>
