cc6fa3cf4561bbeecf9dffcd5df13f8fa568c93a
theopin <theopinto98@hotmail.com>
2023-09-27 20:54:32 +0800

    Replace default dropdown with custom

--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,44 +1,41 @@
 import React from "react";
-import ReactDropdown from "react-dropdown";
 
 import "./Options.css"; // Component-specific CSS
-import "react-dropdown/style.css";
 function Options(props) {
   // Component code here
 
   const regionOptions = [
-    "africa.kenya",
-    "africa.madagascar",
-    "africa.nigeria",
-    "africa.safrica",
-    "asia.india",
-    "asia.malaysia",
-    "asia.singapore",
-    "americas.brazil",
-    "australia",
-    "canada",
-    "europe",
-    "europe.belarus",
-    "europe.denmark",
-    "europe.england",
-    "europe.estonia",
-    "europe.finland",
-    "europe.france",
-    "europe.ireland",
-    "europe.italy",
-    "europe.malta",
-    "europe.netherlands",
-    "europe.poland",
-    "europe.scotland",
-    "europe.slovakia",
-    "europe.sweden",
-    "europe.wales",
-    "meast.sarabia",
-    "nz",
-    "philippines",
-    "usa",
+    { value: 'africa.kenya', label: 'Kenya' },
+    { value: 'africa.madagascar', label: 'Madagascar' },
+    { value: 'africa.nigeria', label: 'Nigeria' },
+    { value: 'africa.safrica', label: 'South Africa' },
+    { value: 'asia.india', label: 'India' },
+    { value: 'asia.malaysia', label: 'Malaysia' },
+    { value: 'asia.singapore', label: 'Singapore' },
+    { value: 'americas.brazil', label: 'Brazil' },
+    { value: 'australia', label: 'Australia' },
+    { value: 'canada', label: 'Canada' },
+    { value: 'europe', label: 'Europe' },
+    { value: 'europe.belarus', label: 'Belarus' },
+    { value: 'europe.denmark', label: 'Denmark' },
+    { value: 'europe.england', label: 'England' },
+    { value: 'europe.estonia', label: 'Estonia' },
+    { value: 'europe.finland', label: 'Finland' },
+    { value: 'europe.france', label: 'France' },
+    { value: 'europe.ireland', label: 'Ireland' },
+    { value: 'europe.italy', label: 'Italy' },
+    { value: 'europe.malta', label: 'Malta' },
+    { value: 'europe.netherlands', label: 'Netherlands' },
+    { value: 'europe.poland', label: 'Poland' },
+    { value: 'europe.scotland', label: 'Scotland' },
+    { value: 'europe.slovakia', label: 'Slovakia' },
+    { value: 'europe.sweden', label: 'Sweden' },
+    { value: 'europe.wales', label: 'Wales' },
+    { value: 'meast.sarabia', label: 'Saudi Arabia' },
+    { value: 'nz', label: 'New Zealand' },
+    { value: 'philippines', label: 'Philippines' },
+    { value: 'usa', label: 'USA' },
   ];
-  const defaultOption = regionOptions[0];
 
   const currentDate = new Date();
   const minDate = new Date();
@@ -46,24 +43,33 @@ function Options(props) {
 
   const maxDate = new Date();
   maxDate.setDate(currentDate.getDate() + 8);
+
+  const regionOptionsDiv = regionOptions.map((option) => (
+    <option key={option.value} value={option.value}>
+      {option.label}
+    </option>
+  ))
   return (
     <div className="options">
-      {/* <ReactDropdown
-        options={regionOptions}
-        onChange={(region) => props.setSelectedRegion(region)}
-        value={props.selectedRegion}
-        placeholder="Select an option"
-      /> */}
+
+      <div className="custom-dropdown">
+        <select
+          id="region"
+          onChange={(e) => props.setSelectedRegion(e.target.value)}
+          value={props.selectedRegion}
+        >
+          {regionOptionsDiv}
+        </select>
+      </div>
 
       <div className="custom-datepicker">
-        <label htmlFor="datepicker">Select a date</label>
         <input
           type="date"
           id="datepicker"
           onChange={(e) => props.setSelectedDate(e.target.value)}
           value={props.selectedDate}
-          min={minDate.toISOString().split('T')[0]}
-          max={maxDate.toISOString().split('T')[0]}
+          min={minDate.toISOString().split("T")[0]}
+          max={maxDate.toISOString().split("T")[0]}
         />
       </div>
     </div>
