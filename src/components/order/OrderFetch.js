68dfdd1253fcaa794c384e8531618d0e185e958e
theopin <theopinto98@hotmail.com>
2023-09-27 23:27:49 +0800

    Fix country selection

--- src/components/order/OrderFetch.js
+++ src/components/order/OrderFetch.js
@@ -2,8 +2,8 @@ import jsonp from "jsonp";
 import moment from "moment";
 function fetchData(region, date) {
   const baseUrl = "https://universalis.com/";
-
-  const url = `${baseUrl}${region.value ? region.value + "/" : ""}${moment(date).format('YYYYMMDD')}/jsonpmass.js`;
+  console.log(region)
+  const url = `${baseUrl}${region ? region + "/" : ""}${moment(date).format('YYYYMMDD')}/jsonpmass.js`;
   console.log(url, 101)
 
   return new Promise((resolve, reject) => {
