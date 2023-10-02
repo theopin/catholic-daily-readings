d774150b0c1f925e92045dccf08a54540ab74a89
theopin <theopinto98@hotmail.com>
2023-10-02 18:07:55 +0800

    refine parsing ability to split by line if too big

--- src/components/responsorial/Responsorial.jsx
+++ src/components/responsorial/Responsorial.jsx
@@ -22,6 +22,35 @@ function decodeHtmlEntities(htmlString) {
   return textContentArray.join(' ');
 }
 
+function combineShortSentences(sentences, minLength) {
+  const combinedSentences = [];
+  let currentSentence = '';
+
+  sentences.forEach((sentence) => {
+    if ((currentSentence.length + sentence.length) < minLength) {
+      // Add the sentence to the currentSentence variable
+      if (currentSentence.length > 0) {
+        currentSentence += ` ${sentence}`;
+      } else {
+        currentSentence = sentence;
+      }
+    } else {
+      // Combine the current sentence with the next one
+      if (currentSentence.length > 0) {
+        combinedSentences.push(currentSentence);
+      }
+      currentSentence = sentence;
+    }
+  });
+
+  // Add the last remaining sentence if it's not empty
+  if (currentSentence.length > 0) {
+    combinedSentences.push(currentSentence);
+  }
+
+  return combinedSentences;
+}
+
 function createFormattedResponse(lines, props) {
   const formattedResponse = [];
   let formattedVerse = [];
@@ -29,19 +58,23 @@ function createFormattedResponse(lines, props) {
   let response = '';
 
   lines.forEach((line, index) => {
+    // Condition 1: line is a response and is not part of the verse
     if (
       index === 0
       || (response.includes(line.trim().toUpperCase())
         && !response.includes(lines[index - 1].trim().toUpperCase()))
     ) {
+      // Condition 1a: line is the not the first time a response is being encountered
       if (index !== 0) {
         formattedResponse.push(
           <div key={`R${props.title}${index + 1}`} className="r-verse">
             {formattedVerse}
           </div>,
         );
+        // reset the verse paragraph
         formattedVerse = [];
       } else {
+        // Condition 1b: line is the first time a response is being encountered
         response = line
           .replace(/\(\d+[a-zA-Z]?\)/g, '')
           .trim()
@@ -54,13 +87,34 @@ function createFormattedResponse(lines, props) {
         </div>,
       );
     } else if (line.trim().startsWith('or') && response.includes(lines[index - 1].trim().toUpperCase())) {
+      // Condition 2: There is an alternative response (starting with or) coming after the response
       formattedResponse.push(
         <div key={`V${props.title}${index + 1}`} className="response">
           {`or R. ${he.decode(line.split('or')[1]).trim()}`}
         </div>,
       );
     } else {
-      formattedVerse.push(<div key={`VL-${index + 1}`}>{he.decode(line).trim()}</div>);
+      // Condition 3: The line is part of the verse paragraph
+      const decodedLine = he.decode(line).trim();
+
+      if (decodedLine.length < 70) {
+        // Condition 3a: Process the line without splitting it
+        formattedVerse.push(<div key={`VL-${index + 1}`}>{he.decode(decodedLine).trim()}</div>);
+      } else {
+        // Condition 3b: Split the line (combine small splits) and then feed it as divs
+        const sentences = decodedLine.match(/[^;:,]+[;:,.!?'"”’)]*(?=\s|$)/g);
+        console.log(101, sentences);
+
+        const formattedSentences = combineShortSentences(sentences, 55);
+
+        formattedSentences.forEach((phrase, phraseIndex) => {
+          formattedVerse.push(
+            <div key={`VL-${index + 1}-${phraseIndex + 1}`}>
+              {phrase}
+            </div>,
+          );
+        });
+      }
     }
   });
   return formattedResponse;
