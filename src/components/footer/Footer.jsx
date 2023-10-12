d9180b42c42274c51fbb83f7bed5edf133fbeb58
theopin <theopinto98@hotmail.com>
2023-10-12 16:50:45 +0800

    Add lighter shade for overall app and modify date

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,13 +3,17 @@ import './Footer.css'; // Component-specific CSS
 
 function Footer() {
   // Component code here
+  const currentDate = new Date();
 
   return (
     // eslint-disable-next-line react/style-prop-object
     <div>
       <hr />
       <div className="footer-text">
-        Copyright &#169; 1996-2023 Universalis Publishing Limited: see
+        Copyright &#169; 1996-
+        {currentDate.getFullYear()}
+        {' '}
+        Universalis Publishing Limited: see
         www.universalis.com. Scripture readings from the Jerusalem Bible are
         published and copyright &#169; 1966, 1967 and 1968 by Darton, Longman
         &amp; Todd, Ltd and Doubleday, a division of Random House, Inc, and used
