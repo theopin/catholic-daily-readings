6804e901a7ec362e2bcc81c98371b785e558aee9
theopin <theopinto98@hotmail.com>
2023-10-13 10:00:36 +0800

    Replace date picker

--- src/components/options/datepickerobject/DatePickerObject.jsx
+++ src/components/options/datepickerobject/DatePickerObject.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import DatePicker from 'react-datepicker';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';
 
 function DatePickerObject(props) {
   const { selectedDate, setSelectedDate } = props;
 
   const currentDate = new Date();
-  const minDate = new Date();
-  minDate.setDate(currentDate.getDate() - 3);
 
-  const maxDate = new Date();
-  maxDate.setDate(currentDate.getDate() + 8);
+  const minDate = moment(new Date().setDate(currentDate.getDate() - 3)).format('YYYY-MM-DD');
+  const maxDate = moment(new Date().setDate(currentDate.getDate() + 8)).format('YYYY-MM-DD');
 
   return (
     // <div className="custom-element">
     <div className="col-auto">
-      <button type="button" className="btn btn-primary">
-        <DatePicker
-          selected={selectedDate}
-          minDate={minDate}
-          maxDate={maxDate}
-          onChange={(date) => setSelectedDate(date)}
-        />
-      </button>
+
+      <input
+        className="form-control"
+        type="date"
+        min={minDate}
+        max={maxDate}
+        value={moment(selectedDate).format('YYYY-MM-DD')}
+        onChange={(e) => setSelectedDate(e.target.value)}
+      />
 
     </div>
   );
